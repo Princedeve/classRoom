dcf16037c4bfee67c47d911472b046f23353e2bc
Prince <prince935059@gmail.com>
2025-11-15 16:07:27 +0530

    update server.js

--- server.js
+++ server.js
@@ -4,23 +4,34 @@ const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const flash = require("connect-flash");
+
+const path = require("path");
+const util = require('util');
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 const sessionOptions = {
     secret: "mysecretstring", 
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: true
 };
+
+
 //middleware
 app.use(session(sessionOptions));
+app.use(flash());
 
 app.get("/register", (req, res) =>{
     let {name = "anonymous"} = req.query;
     req.session.name = name;
+    req.flash("success", "user register successfully");
     res.redirect("/hello");
 });
 
 app.get("/hello", (req, res) =>{
-    res.send(`hello ${req.session.name}`);
+    res.render("page.ejs", {name: req.session.name, msg: req.flash("success")});
 });
 
 // app.get("/reqcount", (req, res) => {
