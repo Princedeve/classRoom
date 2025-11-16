87b80409483901aaefbe79661ad331e3b0acc421
Prince <prince935059@gmail.com>
2025-11-16 19:45:37 +0530

    update server.js

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const posts = require("./routes/post.js");
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const flash = require("connect-flash");
-
 const path = require("path");
-const util = require('util');
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -23,15 +22,25 @@ const sessionOptions = {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use((req, res, next) =>{
+    res.locals.successMsg = req.flash("success");
+    res.locals.errorMsg = req.flash("error");
+        next();
+});
+
 app.get("/register", (req, res) =>{
     let {name = "anonymous"} = req.query;
     req.session.name = name;
-    req.flash("success", "user register successfully");
+    if(name === "anonymous"){
+        req.flash("error", "user not registered");
+    }else{
+        req.flash("success", "user register successfully");
+    } 
     res.redirect("/hello");
 });
 
 app.get("/hello", (req, res) =>{
-    res.render("page.ejs", {name: req.session.name, msg: req.flash("success")});
+    res.render("page.ejs", {name: req.session.name});
 });
 
 // app.get("/reqcount", (req, res) => {
